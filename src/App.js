27ae338236c799ad5eaa41ed35e4ba6450e4508c
HyeonE <mag30102000@gmail.com>
2023-06-29 10:09:36 +0900

    My Todo List 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [memos, setMemos] = useState([
+    { id: 1, text: "react를 배워봅시다" },
+    { id: 2, text: "useState를 배워봅시다" },
+    { id: 3, text: "자바스크립트를 배워봅시다" },
+  ]);
+  const [text, setText] = useState("");
+  const onClick = () => {
+    const newMemo = {
+      id: memos.length + 1,
+      text,
+    };
+    setMemos([...memos, newMemo]);
+    setText("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="template">
+      <div className="insert-zone">
+        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        <button onClick={onClick}>추가하기</button>
+      </div>
+      <h1 className="title-zone">Todo List</h1>
+      <div className="list-zone">
+        {memos.map((memo) => {
+          return (
+            <div className="list" key={memo.id}>
+              {memo.text}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
